e601de60cfc91bee2310c68d81c489131329737e
Barak Tawily <barakt@wix.com>
2019-11-05 16:20:08 +0200

    version bump

--- pyhacks/__init__.py
+++ pyhacks/__init__.py
@@ -8,8 +8,8 @@ Pyhacks library
 """
 
 __title__ = 'pyhacks'
-__version__ = '1.0.11'
-__build__ = 0x010010
+__version__ = '1.0.12'
+__build__ = 0x010012
 __author__ = 'Barak Tawily'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 Barak Tawily'
